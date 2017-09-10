01fd2956b21776d9b40ba0d9a36fa0479cc9ee85
adrianperez <adrian.perez@orangeandbronze.com>
2017-09-10 15:34:43 +0800

    add basic text sentiment analysis

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+
+from services.sentiment_service import get_sentiment
 
 app = Flask(__name__)
-
+app.config.from_object('config.BaseConfig')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/sentiment')
+def get_text_sentiment():
+    return get_sentiment("This pasta sucks");
 
 
 if __name__ == '__main__':
---
+++ services/sentiment_service.py
@@ -0,0 +1,17 @@
+import re
+
+from textblob import TextBlob
+
+
+def __clean_text(text):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).split())
+
+def get_sentiment(text):
+    analysis = TextBlob(__clean_text(text))
+    # set sentiment
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
